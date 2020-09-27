3c124bf389f370f85cf745da1a214bf869de5fb6
Philihp Busby <philihp@gmail.com>
2020-09-27 03:26:36 +0000

    cleanups

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,35 @@
+import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>pgp.philihp.com</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>pgp.philihp.com</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This is a statically hosted PGP keyserver.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://github.com/philihp/pgp.philihp.com"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Source &rarr;</h3>
+            It&apos;s open source so you can learn from my mistakes.
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://philihp.com/pgp" className={styles.card}>
+            <h3>Query &rarr;</h3>
+            It only responds to queries for my key, and keys I keep on my
+            keyring.
           </a>
         </div>
       </main>
@@ -63,3 +47,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home
---
+++ pages/pks/lookup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Lookup = () => (
+  <>
+    <div>hello</div>
+  </>
+)
+
+export default Lookup
