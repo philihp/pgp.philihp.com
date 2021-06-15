09d1a0de7d9b43fab1e63cbd0097ee2e845c64dd
Philihp Busby <philihp@gmail.com>
2021-06-15 17:31:41 +0000

    eslinted

--- pages/stats.js
+++ pages/stats.js
@@ -51,11 +51,11 @@ const Stats = () => {
         <tr valign="top">
           <td>
             <h2>Gossip Peers</h2>
-            <table summary="Gossip Peers"></table>
+            <table summary="Gossip Peers"/>
           </td>
           <td>
             <h2>Outgoing Mailsync Peers</h2>
-            <table summary="Mailsync Peers"></table>
+            <table summary="Mailsync Peers"/>
           </td>
         </tr>
       </table>
